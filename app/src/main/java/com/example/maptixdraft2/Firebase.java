007a73fd854d0279ae797e9c3eeecdd9f42c86a8
kewennnnn <65531086+kewennnnn@users.noreply.github.com>
2020-11-29 20:16:22 +0800

    Update Firebase.java

--- app/src/main/java/com/example/maptixdraft2/Firebase.java
+++ app/src/main/java/com/example/maptixdraft2/Firebase.java
@@ -16,14 +16,47 @@ public class Firebase {
     static DatabaseReference myDatabaseRef = FirebaseDatabase.getInstance().getReference();
 
     public static void addItem(String itemName, String itemQty, String username) {
-        myDatabaseRef.child("Users").child(username).child(itemName).setValue(itemQty);
-        Log.i("Kewen","Item Added! "+itemName+": "+itemQty);
+        if (!itemName.equals("")) {
+            myDatabaseRef.child("Users").child(username).child(itemName).setValue(itemQty);
+            Log.i("Kewen","Item Added! "+itemName+": "+itemQty);
+        }
     }
 
     public static void addItem(ListItem itemObject, String username) {
         String itemName = itemObject.getItems();
         String itemQty = itemObject.getQuantity();
         addItem(itemName, itemQty, username);
+    }
+
+    public static void deleteItem(String itemName, String username) {
+        myDatabaseRef.child("Users").child(username).child(itemName).removeValue();
+        Log.i("Kewen","Item Removed! "+itemName);
+    }
+
+    public static void deleteItem(ListItem itemObject, String username) {
+        String itemName = itemObject.getItems();
+        deleteItem(itemName, username);
+    }
+
+    public static void itemAvailability(final booleanCallbackInterface callbackAction, final String itemName) {
+        DatabaseReference itemsDatabaseReference = myDatabaseRef.child("items");
+        itemsDatabaseReference.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                for (DataSnapshot snapshot : dataSnapshot.getChildren()){ //iterate through the category list
+                    String currentItem = snapshot.getKey();
+                    if (currentItem.equals(itemName)) {
+                        callbackAction.onCallback(true);
+                    }
+                }
+                callbackAction.onCallback(false);
+            }
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
+
     }
 
     public static void displayItemSuggestions(final callbackInterface callbackAction) {
@@ -34,10 +67,8 @@ public class Firebase {
             public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
                 for (DataSnapshot snapshot : dataSnapshot.getChildren()){ //iterate through the category list
                     String currentItem = snapshot.getKey();
-                    Log.i("Kewen items",currentItem);
                     if (!currentItem.equals("entrance")) {
                         itemsArraylist.add(currentItem);
-                        Log.i("Kewen items","items: "+itemsArraylist.toString());
                     }
                 }
                 String[] itemsList = new String[itemsArraylist.size()];
@@ -53,7 +84,7 @@ public class Firebase {
         });
     };
 
-    public static void displayShoppingList(final userhelperCallbackInterface callbackAction, String username) {
+    public static void displayShoppingList(final listitemCallbackInterface callbackAction, String username) {
         DatabaseReference userDatabaseReference = myDatabaseRef.child("Users").child(username);
         userDatabaseReference.addValueEventListener(new ValueEventListener() {
             @Override
@@ -81,8 +112,11 @@ public class Firebase {
     interface callbackInterface { // equivalent of Bar, a nested interface
         void onCallback(String[] myList);
     }
-    interface userhelperCallbackInterface {
+    interface listitemCallbackInterface {
         void onCallback(List<ListItem> myList);
+    }
+    interface booleanCallbackInterface {
+        void onCallback(boolean bool);
     }
 
 }
